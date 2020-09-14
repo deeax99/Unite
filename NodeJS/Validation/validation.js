c84f319bbd565f4d450c18b7cc60d2fcdb6ccb59
Deea Majed <deea1887@yahoo.com>
2020-09-14 13:33:57 +0300

    Validation

--- NodeJS/Validation/validation.js
+++ NodeJS/Validation/validation.js
@@ -1,19 +1,25 @@
-module.exports = class {
-
-    email(object){
-        
+const passwordStrength = require('check-password-strength')
+const md5 = require('md5');
+module.exports = class 
+{
+    EmailValidatoinCheck(email)
+    {
+        const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        return emailRegexp.test(email)
     }
 
-    password(object){
-
+    PasswordCheck (password)
+    {
+        return passwordStrength(password).value != 1
     }
 
-    post(object){
+    post(object)
+    {
 
     }
-
-    hash(object){
-        
+    Hash128(text)
+    {
+        return md5(text)
     }
 
 }
